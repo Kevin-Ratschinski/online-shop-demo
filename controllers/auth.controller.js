06c4975d6cd29fef1f408e0864905fd21d312410
Kevin Ratschinski <k.ratschinski@icloud.com>
2022-03-26 17:49:21 +0100

    implement authentication

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,53 @@
+const User = require('../models/user.model');
+const authUtil = require('../util/authentication');
+
 function getSignup(req, res) {
   res.render('customer/auth/signup');
 }
-function getLogin(req, res) {}
+
+async function signup(req, res) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.fullname,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+
+  await user.signup();
+
+  res.redirect('/login');
+}
+
+function getLogin(req, res) {
+  res.render('customer/auth/login');
+}
+
+async function login(req, res) {
+  const user = new User(req.body.email, req.body.password);
+  const existingUser = await user.getUserWithSameEmail();
+
+  if (!existingUser) {
+    res.redirect('/login');
+    return;
+  }
+
+  const passwordIsCorrect = await user.comparePassword(existingUser.password);
+
+  if (!passwordIsCorrect) {
+    res.redirect('/login');
+    return;
+  }
+
+  authUtil.createUserSession(req, existingUser, function () {
+    res.redirect('/');
+  });
+}
 
 module.exports = {
   getSignup: getSignup,
   getLogin: getLogin,
+  signup: signup,
+  login: login,
 };
